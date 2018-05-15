66f522e300c831ede75f0715248a1614404b195e
jiangmengmeng <1804226997@qq.com>
2018-05-15 11:27:29 +0800

    Update disk.h

--- disk.h
+++ disk.h
@@ -1,3 +1,25 @@
+// #pragma once
+
+// #include <string>
+// #include <fstream>
+// #include <iostream>
+// using namespace std;
+
+// class Disk {
+// private:
+// 	const string disk_file;
+// 	int BLKsize;		// size of a single block
+// public:
+// 	Disk();
+// 	~Disk();
+
+// 	bool GETABLOCK(char buf[], int block_num);
+// 	bool GETPBLOCK(char buf[], int block_num);
+// 	bool GETPBLOCK(char buf[], int block_num, int l);
+// 	bool PUTABLOCK(char buf[], int block_num);
+// 	bool PUTPBLOCK(char buf[], int block_num);
+// 	bool PUTPBLOCK(char buf[], int block_num, int l);
+// };
 #pragma once
 
 #include <string>
@@ -8,15 +30,11 @@ using namespace std;
 class Disk {
 private:
 	const string disk_file;
-	int BLKsize;		// size of a single block
+	int blocksize;		// size of a single block
 public:
 	Disk();
 	~Disk();
 
-	bool GETABLOCK(char buf[], int block_num);
-	bool GETPBLOCK(char buf[], int block_num);
-	bool GETPBLOCK(char buf[], int block_num, int l);
-	bool PUTABLOCK(char buf[], int block_num);
-	bool PUTPBLOCK(char buf[], int block_num);
-	bool PUTPBLOCK(char buf[], int block_num, int l);
+	bool GETBLOCK(char buf[], int block_num, int offset, int length);
+	bool PUTBLOCK(char buf[], int block_num, int offset, int length);
 };
